4d55d878f0ba126a2c0b4b03941df854a4e9fe19
Felipe Castro <fpocastro@gmail.com>
2021-09-16 23:55:34 -0300

    Removed useColorModeValue hook

--- src/hooks/useColorModeValue/useColorModeValue.ts
+++
@@ -1,7 +0,0 @@
-import { useTheme } from "../../contexts/ThemeProvider";
-
-export const useColorModeValue = <T>(lightModeValue: T, darkModeValue: T) => {
-  const { colorMode } = useTheme();
-
-  return colorMode === "light" ? lightModeValue : darkModeValue;
-};
