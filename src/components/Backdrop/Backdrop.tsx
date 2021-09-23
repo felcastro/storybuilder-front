59c634c7911f7430cd6de0de10c28c13513938eb
Felipe Castro <fpocastro@gmail.com>
2021-09-22 21:13:35 -0300

    Updated backdrop animation and typing

--- src/components/Backdrop/Backdrop.tsx
+++ src/components/Backdrop/Backdrop.tsx
@@ -1,22 +1,46 @@
-import styled, { StyledComponentPropsWithRef } from "styled-components";
+import { motion, Variants } from "framer-motion";
+import { HTMLAttributes } from "react";
+import styled from "styled-components";
+import { toRgba } from "../../utils";
 
-const StyledBackdrop = styled.div`
+const variants: Variants = {
+  visible: {
+    opacity: 1,
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+
+const StyledBackdrop = styled(motion.div).attrs({
+  variants,
+  initial: "hidden",
+  animate: "visible",
+  exit: "hidden",
+  transition: { duration: 0.05 },
+})`
   position: fixed;
   top: 0;
   left: 0;
   z-index: ${({ theme }) => theme.zIndices.modal};
   width: 100%;
   height: 100%;
-  background: ${({ theme }) => theme.colors.black}e1;
+  background: ${({ theme }) =>
+    theme.mode(
+      toRgba(theme.colors.black, 0.6),
+      toRgba(theme.colors.black, 0.6)
+    )};
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
-type BackdropProps = StyledComponentPropsWithRef<"div">;
-
-export const Backdrop = ({ onClick, children, ...props }: BackdropProps) => (
-  <StyledBackdrop onClick={onClick} {...props}>
+export const Backdrop = ({
+  onClick,
+  onMouseDown,
+  children,
+}: HTMLAttributes<HTMLDivElement>) => (
+  <StyledBackdrop onClick={onClick} onMouseDown={onMouseDown}>
     {children}
   </StyledBackdrop>
 );
